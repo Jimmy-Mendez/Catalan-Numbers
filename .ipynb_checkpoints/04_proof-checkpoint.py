f936d85d02af40976094a69f2fed3d3b49dd0fdd
Jimmy Mendez <jmen34956@gmail.com>
2022-07-14 17:32:39 -0400

    add flip scene

--- .ipynb_checkpoints/04_proof-checkpoint.py
+++ .ipynb_checkpoints/04_proof-checkpoint.py
@@ -5,6 +5,7 @@ class proof(Scene):
         dots = dict()
         var_index = 0
         line = DashedLine(start= np.array([-2., -2., 0.]), end=np.array([2, 2., 0.]), buff=0, color = '#FC6255')
+        line_p = DashedLine(start= np.array([-2., -1., 0.]), end=np.array([1, 2., 0.]), buff=0, color = '#f48fff')
         pos_paths = Tex("{2n \choose n}")
         move_dot = Dot(np.array([-2, -2., 0]), radius=0.15, color= '#69cdff')
         for x in range(-2, 3):
@@ -20,14 +21,37 @@ class proof(Scene):
         points_3 = [[1, 0, 0],[1, 0, 0],[0, 1, 0],[0, 1, 0],[0, 1, 0],[1, 0, 0],[0, 1, 0],[1, 0, 0]]
         
         # showing possible paths
+        path_1 = []
+        path_2 = []
+        path_3 = []
+        start = np.array([-2.,-2.,0])
         for point in points_1:
+            end = np.add(start, point)
             self.play(ApplyMethod(move_dot.shift, np.array(point)), run_time = 0.3)
+            path_1.append(Line(start= start, end= end, buff=0, color = '#ffeb69'))
+            self.play(ShowCreation(path_1[-1], scale=0.5, run_time = 0.28))
+            start = np.array(end)
+        self.play(Uncreate(Group(*path_1), scale=0.5, run_time = 0.28))
         self.play(ApplyMethod(move_dot.move_to, np.array([-2., -2., 0.])), run_time = 0.3)
+        start = np.array([-2.,-2.,0])
         for point in points_2:
+            end = np.add(start, point)
             self.play(ApplyMethod(move_dot.shift, np.array(point)), run_time = 0.3)
+            path_2.append(Line(start= start, end= end, buff=0, color = '#ffeb69'))
+            self.play(ShowCreation(path_2[-1], scale=0.5, run_time = 0.28))
+            start = np.array(end)
+        save_path = Group(*path_2)
+        save_path.save_state()
+        self.play(Uncreate(Group(*path_2), scale=0.5, run_time = 0.28))
         self.play(ApplyMethod(move_dot.move_to, np.array([-2., -2., 0.])), run_time = 0.3)
+        start = np.array([-2.,-2.,0])
         for point in points_3:
+            end = np.add(start, point)
             self.play(ApplyMethod(move_dot.shift, np.array(point)), run_time = 0.3)
+            path_3.append(Line(start= start, end= end, buff=0, color = '#ffeb69'))
+            self.play(ShowCreation(path_3[-1], scale=0.5, run_time = 0.28))
+            start = np.array(end)
+        self.play(Uncreate(Group(*path_3), scale=0.5, run_time = 0.28))
         
         #showing possible up strokes   
         
@@ -58,3 +82,19 @@ class proof(Scene):
             self.play(ShowCreation(group_lines, scale=0.5, run_time = 0.28))
             self.wait(0.5)
             self.play(Uncreate(group_lines, scale=0.5, run_time = 0.28))
+        self.play(ShowCreation(line, scale=0.5, run_time = 0.28))
+        self.play(line.animate.scale(1.25))
+        self.play(line.animate.scale(.8))
+        save_path.restore()
+        self.play(ShowCreation(save_path, scale=0.5, run_time = 0.28))
+        self.play(ShowCreation(line_p, scale=0.5, run_time = 0.28))
+        flipped_points = [[-2,-1,0],[-1,-1,0],[-1,0,0],[0,0,0],[1,0,0],[1,1,0],[1,2,0],[1,3,0]]
+        flipped_path = []
+        count = 1
+        for point in flipped_points:
+            if point != [1,3,0]:
+                flipped_path.append(Line(start = np.array(point), end=np.array(flipped_points[count]), buff=0, color = '#ff6a00'))
+            count+=1
+        group_lines_f = Group(*flipped_path)
+        self.play(ShowCreation(group_lines_f, scale=0.5, run_time = 0.28))
+            
